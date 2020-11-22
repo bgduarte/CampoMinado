2d3db05afed74c80b59d73fd2db0cba8b0c8643b
Bernardo Duarte <bernardogomesduarte@gmail.com>
2020-11-22 17:13:07 -0300

    Feita base para função de renderização.

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
+
+
+
 import pygame
-from pygame.locals import *
  
 class CampoMinadoApp:
     def __init__(self):
-        self._running = True
-        self._display_surf = None
+        self.__running = True
+        self.__screen = None
         self.size = self.weight, self.height = 640, 400
  
     def on_init(self):
         pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
-        self._running = True
+        print('Init')
+        self.__screen = pygame.display.set_mode(self.size)
+        self.__running = True
  
     def on_event(self, event):
         if event.type == pygame.QUIT:
-            self._running = False
+            self.__running = False
     def on_loop(self):
         pass
     def on_render(self):
-        pass
+        red = (255, 0, 0) #Remover mais tarde: Apenas para teste
+        self.__screen.fill(red) #Remover mais tarde: Apenas para teste
+        #Chamadas das funções de renderização
+
+        pygame.display.flip()
+
+
     def on_cleanup(self):
         pygame.quit()
  
     def on_execute(self):
         if self.on_init() == False:
-            self._running = False
+            self.__running = False
  
-        while( self._running ):
+        while( self.__running ):
             for event in pygame.event.get():
                 self.on_event(event)
             self.on_loop()
